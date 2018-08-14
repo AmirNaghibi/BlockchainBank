71415e9aaf738cf4eb3719f86cd894bfe1cf4373
Amir Naghibi <amirmahdi76@gmail.com>
2018-08-13 21:23:31 -0700

    added private variables and construcor for Block class

--- Block.java
+++ Block.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class Block{
+
+    private int previousHash;
+    // Each transaction is a string. A block has a list of transaction
+    private String[] transactions;
+
+    private int blockHash;
+
+    public Block(int previousHash, String[] transactions){
+        this.previousHash = previousHash;
+        this.transactions = transactions;
+
+        Object[] contents = {Arrays.hashCode(transactions), previousHash};
+        this.blockHash = Arrays.hashCode(contents);
+    }
+    
+
     public static void main(String[] args) {
-        System.out.println("Block Class");
     }
 }
